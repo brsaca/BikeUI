f88dcf7e75d7bd1298cc3f266853a9729d9befb8
brsaca <brsaca@gmail.com>
2023-10-13 10:05:17 -0600

    Initial Commit

---
+++ BikeUI/BikeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeUIApp.swift
+//  BikeUI
+//
+//  Created by Brenda Saavedra Cantu on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BikeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeUI
+//
+//  Created by Brenda Saavedra Cantu on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
