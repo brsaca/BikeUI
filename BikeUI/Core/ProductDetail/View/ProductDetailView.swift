21d6ddb6d598b0bb46f1926999a1bba66c214663
brsaca <brsaca@gmail.com>
2023-10-16 12:53:08 -0600

    feat: full design in detail view

--- BikeUI/Core/ProductDetail/View/ProductDetailView.swift
+++ BikeUI/Core/ProductDetail/View/ProductDetailView.swift
@@ -75,7 +75,7 @@ extension ProductDetailView {
             .safeAreaPadding(.horizontal, 20)
             
             PageControl(numberOfPages: 3, currentPage: $currentPage)
-                            .padding(.top, 8)
+                .padding(.top, 8)
         }
         .padding(.top, 30)
     }
@@ -83,63 +83,75 @@ extension ProductDetailView {
     var Information: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
-                HStack(alignment: .center, spacing: 10) {
-                    Text("Description")
-                        .font(.poppins(.bold, size: 18))
-                        .foregroundColor(Color(red: 0.22, green: 0.71, blue: 0.91))
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-                .background(Color(red: 0.21, green: 0.25, blue: 0.33))
-                .cornerRadius(10)
-                .shadow(color: .black.opacity(0.25), radius: 5, x: 4, y: 4)
-                .shadow(color: .white.opacity(0.25), radius: 5, x: -4, y: -4)
-                
                 Spacer()
-                
-                HStack(alignment: .center, spacing: 10) {
-                    Text("Suscription")
-                        .font(.poppins(.regular, size: 18))
-                        .foregroundColor(.white.opacity(0.8))
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-                .background(Color(red: 0.21, green: 0.25, blue: 0.33))
-                .cornerRadius(10)
-                .shadow(color: .black.opacity(0.25), radius: 5, x: 4, y: 4)
-                .shadow(color: .white.opacity(0.25), radius: 5, x: -4, y: -4)
+                ButtonWithText(title: "Description", kind: .primaryOption)
+                Spacer()
+                ButtonWithText(title: "Suscription", kind: .secoundaryOption)
+                Spacer()
             }
+            .padding(.vertical, 20)
+            
+            Text(item.model)
+                .font(.poppins(.bold, size: 17))
+                .padding(.leading, 20)
+                .foregroundColor(.white)
+            
+            Text(item.details)
+                .font(.poppins(.regular, size: 15))
+                .foregroundColor(.white.opacity(0.6))
+                .padding(.horizontal, 20)
+            
             Spacer()
+            
+            BottomBar
+                .padding(.vertical)
+                .padding(.leading, -5)
         }
         .frame(width: .infinity, height: 450)
+        .background(
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.21, green: 0.25, blue: 0.33), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.13, green: 0.16, blue: 0.2), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.02, y: 0.03),
+                endPoint: UnitPoint(x: 0.45, y: 0.38)
+            )
+        )
         .cornerRadius(20)
-       /* .background(
-          LinearGradient(
-            stops: [
-              Gradient.Stop(color: Color(red: 0.21, green: 0.25, blue: 0.33), location: 0.00),
-              Gradient.Stop(color: Color(red: 0.13, green: 0.16, blue: 0.2), location: 1.00),
-            ],
-            startPoint: UnitPoint(x: 0.02, y: 0.03),
-            endPoint: UnitPoint(x: 0.45, y: 0.38)
-          )
-        ) */
         .padding(.leading, -10)
     }
     
-    var Information2: some View {
-        Rectangle()
-          .frame(width: .infinity, height: 450)
+    var BottomBar: some View {
+        HStack {
+            Text(item.formattedAmount)
+                .font(.poppins(.regular, size: 24))
+              .foregroundColor(Color(red: 0.22, green: 0.71, blue: 0.91))
+              .padding(.leading, 30)
+              
+            Spacer()
+            
+            ButtonWithText(title: "Add To Cart", kind: .secoundaryAction)
+                .padding(.trailing, 30)
+        }
+          .frame(width: .infinity, height: 104)
           .background(
             LinearGradient(
               stops: [
                 Gradient.Stop(color: Color(red: 0.21, green: 0.25, blue: 0.33), location: 0.00),
                 Gradient.Stop(color: Color(red: 0.13, green: 0.16, blue: 0.2), location: 1.00),
               ],
-              startPoint: UnitPoint(x: 0.02, y: 0.03),
-              endPoint: UnitPoint(x: 0.45, y: 0.38)
+              startPoint: UnitPoint(x: 0.46, y: 0),
+              endPoint: UnitPoint(x: 0.47, y: 0.59)
             )
           )
-          .cornerRadius(20)
+          .cornerRadius(50)
+          .shadow(color: Color(red: 0.11, green: 0.13, blue: 0.18).opacity(0.25), radius: 20, x: 0, y: -10)
+          .overlay(
+            RoundedRectangle(cornerRadius: 50)
+              .inset(by: 1)
+              .stroke(.clear, lineWidth: 2)
+          )
     }
 }
 
